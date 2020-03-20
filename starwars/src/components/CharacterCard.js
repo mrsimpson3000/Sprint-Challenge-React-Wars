c5252f4df3390f6f06b8a64a0263ef384fd0722c
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-03-20 13:43:53 -0400

    Should be MVP. I'm trying ReactStrap now.

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -1,10 +1,36 @@
 import React from "react";
-import {
-  Card,
-  CardHeader,
-  CardText,
-  CardBody,
-  CardSubtitle,
-  Badge,
-  Col
-} from "reactstrap";
+import styled from "styled-components";
+
+const MyContainer = styled.div`
+  /* background: black; */
+  /* width: 100%; */
+  max-width: 400px;
+  color: black;
+  margin: 20px auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  color: white;
+  box-shadow: 0px 1px 6px -2px grey;
+`;
+
+const CharacterCard = props => {
+  return (
+    <MyContainer>
+      <h1 style={{ color: "black" }}>{props.character.name}</h1>
+      <p style={{ color: "darkblue" }}>Height: {props.character.height}</p>
+      <p style={{ color: "darkblue" }}>Weight: {props.character.mass}</p>
+      <p style={{ color: "darkblue" }}>
+        Hair Color: {props.character.hair_color}
+      </p>
+      <p style={{ color: "darkblue" }}>
+        Eye Color: {props.character.eye_color}
+      </p>
+      <p style={{ color: "darkblue" }}>DOB: {props.character.birth_year}</p>
+      <p style={{ color: "darkblue" }}>Gender: {props.character.gender}</p>
+    </MyContainer>
+  );
+};
+
+export default CharacterCard;
